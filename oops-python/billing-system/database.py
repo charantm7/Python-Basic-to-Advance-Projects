5cc8255c965867c9d5fd82c5dd30b85959d605e0
charantm <charantm8787@gmail.com>
2025-06-21 08:15:35 +0530

    updated

--- oops-python/billing-system/database.py
+++ oops-python/billing-system/database.py
@@ -8,7 +8,7 @@ class BillingDB:
         self.cur = self.connect.cursor()
 
     def create_table(self):
-        self.connect.execute(
+        self.cur.execute(
             """
         CREATE TABLE IF NOT EXISTS PRODUCTS(product_id INTEGER PRIMARY KEY AUTOINCREMENT,
         product_name TEXT, 
@@ -19,7 +19,7 @@ class BillingDB:
         """
         )
 
-        self.connect.execute(
+        self.cur.execute(
             """
         CREATE TABLE IF NOT EXISTS CUSTOMER(customer_id INTEGER PRIMARY KEY AUTOINCREMENT,
         name TEXT,
@@ -28,7 +28,7 @@ class BillingDB:
         """
         )
 
-        self.connect.execute(
+        self.cur.execute(
             """
         CREATE TABLE IF NOT EXISTS BILL(bill_id INTEGER PRIMARY KEY AUTOINCREMENT,
         customer_id INTEGER, data_time TEXT NOT NULL, total_price REAL NOT NULL, FOREIGN KEY (customer_id) REFERENCES CUSTOMER(customer_id)
@@ -36,7 +36,7 @@ class BillingDB:
         """
         )
 
-        self.connect.execute(
+        self.cur.execute(
         
         """
         CREATE TABLE IF NOT EXISTS BILL_ITEM(items_id INTEGER PRIMARY KEY AUTOINCREMENT, bill_id INTEGER, product_id INTEGER, quantity INTEGER NOT NULL, price REAL NOT NULL, 
@@ -45,48 +45,173 @@ class BillingDB:
         """
         )
 
-        self.connect.commit()
+        self.cur.commit()
 
     def close(self):
-        self.connect.close()
+        self.cur.close()
 
     def add_products(self, product_name, category, price, quantity):
-        self.connect.execute("""
+        self.cur.execute("""
         INSERT INTO PRODUCTS( product_name, category, price, quantity) VALUES (?,?,?,?,?)
         """, ( product_name, category, price, quantity))
-        self.connect.commit()
+        self.cur.commit()
+
+    def view_products(self):
+        products = self.cur.execute("""
+        SELECT * FROM PRODUCTS
+        """)
+        return products.fetchall()
     
+    def edit_products(self, product_id):
+        print("What do want to edit?")
+        print("1. All\n2. Name\n3. Price\n4. Stock Quantity\n5. Category\n6. Exit")
+        choice = int(input("Enter the choice: "))
+
+        if choice ==1:
+            new_name = input("Enter the New Name: ")
+            new_price = int(input("Enter the New Price: "))
+            new_stock = int(input("Enter the New Stock Quantity: "))
+            new_category = input("Enter the New category: ")
+
+            self.cur.execute("""
+            UPDATE PRODUCTS SET product_name=?, category=?, price=?, quantity=? WHERE product_id=?
+            """,(new_name,new_category, new_price, new_stock, product_id))
+
+        elif choice == 2:
+            new_name = input("Enter the New Name: ")
+
+            self.cur.execute("""
+            UPDATE PRODUCTS SET product_name=? WHERE product_id=?
+            """,(new_name, product_id,))
+        
+        elif choice == 3:
+            new_price = int(input("Enter the New Price: "))
+
+            self.cur.execute("""
+            UPDATE PRODUCTS SET   price=? WHERE product_id=?
+            """,( new_price, product_id,))
+
+        elif choice == 4:
+            new_stock = int(input("Enter the New Stock Quantity: "))
+
+            self.cur.execute("""
+            UPDATE PRODUCTS SET quantity=? WHERE product_id=?
+            """,(new_stock, product_id,))
+
+        elif choice == 5:
+            new_category = input("Enter the New category: ")
+
+            self.cur.execute("""
+            UPDATE PRODUCTS SET category=? WHERE product_id=?
+            """,(new_category, product_id,))
+        
+        elif choice == 6:
+            print("Exiting...")
+            return None
+        else:
+            print("Invalid Choice!")
+            return
+        
+        self.cur.commit()
+
+
+    def delete_product(self, product_id):
+        self.cur.execute("""SELECT * FROM PRODUCTS WHERE product_id=?""", (product_id,))
+        fetched = self.cur.fetchone()
+
+        if not fetched:
+            print(f"No product with this id: {product_id}")
+
+        else:
+            self.cur.execute("""DELETE FROM PRODUCTS WHERE product_id=?""", (product_id,))
+            print(f"Product with id: {product_id} has Deleted!")
+
+        self.cur.commit()
+
+
     def get_or_add_customers(self, name, phone, email ):
-        self.connect.execute("""SELECT customer_id FROM CUSTOMER WHERE name = ?, phone = ?, email = ?""", (name, phone, email))
+        self.cur.execute("""SELECT customer_id FROM CUSTOMER WHERE name = ?, phone = ?, email = ?""", (name, phone, email))
         result = self.cur.fetchone()
         if result:
             return result[0]
         else:
-            self.connect.execute("""
+            self.cur.execute("""
             INSERT INTO CUSTOMER(name, phone, email)VALUES(?,?,?,?)
             """, (name, phone, email))
-            self.connect.commit()
+            self.cur.commit()
             return self.cur.lastrowid
+        
+    def view_all_customers(self):
+        fetch = self.cur.execute("""SELETCT * FROM CUSTOMER""")
+        return fetch.fetchall()
 
 
     def create_bill(self, customer_id, total_price):
         date_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        self.connect.execute("""
+        self.cur.execute("""
         INSERT INTO BILL(customer_id, date_time, total_price)
         """, (customer_id, date_time, total_price))
-        self.connect.commit()
-        self.cur.lastrowid
+        self.cur.commit()
+        return self.cur.lastrowid
 
-    def bill_for_items(self, bill_id, product_id, quantity, price):
-        self.connect.execute("""
+    def bill_for_items(self, bill_id, product_id, total_quantity):
+        self.cur.execute("""
         SELECT price, quantity FROM PRODUCTS WHERE product_id = ?
         """, (product_id))
+        product = self.cur.fetchone()
 
-        prod
-        self.connect.execute("""
+        if not product:
+            print("No product available!")
+
+        if total_quantity > quantity:
+            print("No Available stocks")
+
+
+        price , quantity = product
+        
+        total_price = total_quantity * price
+       
+        self.cur.execute("""
         INSERT INTO BILL(bill_id, product_id, quantity, price)
-        """, (bill_id, product_id, quantity, price))
-        self.connect.commit()
+        """, (bill_id, product_id, total_quantity, total_price))
+        self.cur.commit()
+        
+        new_stock = quantity - total_quantity
+
+        self.cur.execute("""
+                             UPDATE PRODUCTS SET quantity=? WHERE product_id=?""",(new_stock, product_id))
+
+        self.cur.commit()
+
+    def view_past_bill(self):
+        last = self.cur.execute("""SELECT * FROM BILL ORDER BY bill_id DESC LIMIT 1""")
+        return last.fetchone()
+    
+
+    def list_low_stock_product(self):
+        print("1. Enter my own threshold to check stock quantity.")
+        print("2. Give last 10 product which has low stock.")
+        choice = int(input("Enter your choice [1,2]: "))
+
+        if choice == 1:
+            threshold = int(input("Enter the threshold number: "))
+            return self.cur.execute("""SELECT * FROM PRODUCTS WHERE quantity < ?""", (threshold,)).fetchall()
+
+        elif choice == 2:
+            return self.cur.execute("""SELECT * FROM PRODUCTS ORDER BY quantity ASC LIMIT 10""")
+        
+        else:
+            print("Inavlid choice!")
+            return
+        
+        
+            
+
+        
+    
+
+
+
 
 
 
